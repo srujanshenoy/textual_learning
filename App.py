38f9c12126c4a1cb0a766908260a6ef3ce0ae58c
srujanshenoy <srujanshenoy@gmail.com>
2025-04-19 09:32:28 +0530

    Commiting again

--- App.py
+++ App.py
@@ -1,9 +1,48 @@
-from textual.app import App
-from textual.widgets import Footer, Header
-
-class stopwatch(App):
-    def compose(self):
+from textual.app import App, ComposeResult
+from textual.containers import HorizontalGroup, VerticalScroll
+from textual.widgets import Button, Digits, Footer, Header
 
 
-if __name__ == '__main__':
-    stopwatch().run()
+class TimeDisplay(Digits):
+    """A widget to display elapsed time."""
+    pass
+
+
+class Stopwatch(HorizontalGroup):
+    """A stopwatch widget."""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets of a stopwatch."""
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
+        yield TimeDisplay("00:00:00.00")
+
+    def on_button_pressed(self, event:Button.Pressed) -> None:
+        """Change the CSS Class(es) to incorporate started"""
+        if self.button.id == "start":
+            
+
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    CSS_PATH = 'App.css'
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch())
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+
+if __name__ == "__main__":
+    app = StopwatchApp()
+    app.run()
